4bbeaadf08b4c931f5af4d7337d4433b410eb428
Thomas Heaney <thomas.heaney@barrheadtravel.co.uk>
2017-05-23 16:51:12 +0100

    Finished Part 2

---
+++ FionaWhitfieldArt/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace FionaWhitfieldArt.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Home/";
+
+        public ActionResult RenderFeatured()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER  + "_Featured.cshtml");
+        }
+
+        public ActionResult RenderServices()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Services.cshtml");
+        }
+
+        public ActionResult RenderBlog()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Blog.cshtml");
+        }
+
+        public ActionResult RenderClients()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Clients.cshtml");
+        }
+    }
+}
---
+++ FionaWhitfieldArt/Controllers/SiteLayoutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace FionaWhitfieldArt.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
+        public ActionResult RenderHeader()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
+        }
+
+        public ActionResult RenderIntro()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Intro.cshtml");
+        }
+
+        public ActionResult RenderFooter()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
+        }
+
+    }
+}
