90b8420765c44c2073de4a45d9af46d7690e1608
Thomas Heaney <thomas.heaney@barrheadtravel.co.uk>
2017-05-24 11:16:42 +0100

    Finished Part 4

--- FionaWhitfieldArt/Controllers/SiteLayoutController.cs
+++ FionaWhitfieldArt/Controllers/SiteLayoutController.cs
@@ -1,20 +1,19 @@
-﻿using System;
+﻿using FionaWhitfieldArt.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Umbraco.Core.Models;
 using Umbraco.Web.Mvc;
+using System.Runtime.Caching;
 
 namespace FionaWhitfieldArt.Controllers
 {
     public class SiteLayoutController : SurfaceController
     {
         private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
-        public ActionResult RenderHeader()
-        {
-            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
-        }
-
+       
         public ActionResult RenderIntro()
         {
             return PartialView(PARTIAL_VIEW_FOLDER + "_Intro.cshtml");
@@ -23,6 +22,77 @@ namespace FionaWhitfieldArt.Controllers
         public ActionResult RenderFooter()
         {
             return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
+        }
+
+        /// <summary>
+        /// Renders the top navigation in the header partial
+        /// </summary>
+        /// <returns>Partial view with a model</returns>
+        public ActionResult RenderHeader()
+        {
+            List<NavigationListItem> nav = GetObjectFromCache<List<NavigationListItem>>("mainNav", 5, GetNavigationModelFromDatabase);
+
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml", nav);
+        }
+
+
+        /// <summary>
+        /// Finds the home page and gets the navigation structure based on it and it's children
+        /// </summary>
+        /// <returns>A List of NavigationListItems, representing the structure of the site.</returns>
+        private List<NavigationListItem> GetNavigationModelFromDatabase()
+        {
+            const int HOME_PAGE_POSITION_IN_PATH = 1;
+            int homePageId = int.Parse(CurrentPage.Path.Split(',')[HOME_PAGE_POSITION_IN_PATH]);
+            IPublishedContent homePage = Umbraco.Content(homePageId);
+            List<NavigationListItem> nav = new List<NavigationListItem>();
+            nav.Add(new NavigationListItem(new NavigationLink(homePage.Url, homePage.Name)));
+            nav.AddRange(GetChildNavigationList(homePage));
+            return nav;
+        }
+
+        /// <summary>
+        /// Loops through the child pages of a given page and their children to get the structure of the site.
+        /// </summary>
+        /// <param name="page">The parent page which you want the child structure for</param>
+        /// <returns>A List of NavigationListItems, representing the structure of the pages below a page.</returns>
+        private List<NavigationListItem> GetChildNavigationList(dynamic page)
+        {
+            List<NavigationListItem> listItems = null;
+            var childPages = page.Children.Where("Visible");
+            if(childPages != null && childPages.Any() && childPages.Count() > 0)
+            {
+                listItems = new List<NavigationListItem>();
+                foreach(var childPage in childPages)
+                {
+                    NavigationListItem listItem = new NavigationListItem(new NavigationLink(childPage.Url, childPage.Name));
+                    listItem.Items = GetChildNavigationList(childPage);
+                    listItems.Add(listItem);
+                }
+            }
+            return listItems;
+        }
+
+        /// <summary>
+        /// A generic function for getting and setting objects to the memory cache.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be returned.</typeparam>
+        /// <param name="cacheItemName">The name to be used when storing this object in the cache.</param>
+        /// <param name="cacheTimeInMinutes">How long to cache this object for.</param>
+        /// <param name="objectSettingFunction">A parameterless function to call if the object isn't in the cache and you need to set it.</param>
+        /// <returns>An object of the type you asked for</returns>
+        private static T GetObjectFromCache<T>(string cacheItemName, int cacheTimeInMinutes, Func<T> objectSettingFunction)
+        {
+            ObjectCache cache = MemoryCache.Default;
+            var cachedObject = (T)cache[cacheItemName];
+            if (cachedObject == null)
+            {
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(cacheTimeInMinutes);
+                cachedObject = objectSettingFunction();
+                cache.Set(cacheItemName, cachedObject, policy);
+            }
+            return cachedObject;
         }
 
     }
---
+++ FionaWhitfieldArt/Models/NavigationListItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FionaWhitfieldArt.Models
+{
+    public class NavigationListItem
+    {
+        public string Text { get; set; }
+        public NavigationLink Link { get; set; }
+        public List<NavigationListItem> Items { get; set; }
+        public bool HasChildren { get { return Items != null && Items.Any() && Items.Count > 0;  } }
+
+        public NavigationListItem()
+        { }
+
+        public NavigationListItem(NavigationLink link)
+        {
+            Link = link;
+        }
+
+        public NavigationListItem(string text)
+        {
+            Text = text;
+        }
+    }
+}
+
+
