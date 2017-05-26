9e007443cde971517a286eb439507e7922a3da9b
Thomas Heaney <thomas.heaney@barrheadtravel.co.uk>
2017-05-26 14:52:47 +0100

    Finished Part 12

---
+++ FionaWhitfieldArt/Models/SearchGroup.cs
@@ -0,0 +1,15 @@
+﻿
+namespace FionaWhitfieldArt.Models
+{
+    public class SearchGroup
+    {
+        public string[] FieldsToSearchIn { get; set; }
+        public string[] SearchTerms { get; set; }
+
+        public SearchGroup(string[] fieldsToSearchIn, string[] searchTerms)
+        {
+            FieldsToSearchIn = fieldsToSearchIn;
+            SearchTerms = searchTerms;
+        }
+    }
+}
---
+++ FionaWhitfieldArt/Models/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace FionaWhitfieldArt.Models
+{
+    public class SearchViewModel
+    {
+        public string SearchTerm { get; set; }
+        public string DocTypeAliases { get; set; }
+        public string FieldPropertyAliases { get; set; }
+        public int PageSize { get; set; }
+        public int PagingGroupSize { get; set; }
+        public string Category { get; set; }
+        public List<SearchGroup> SearchGroups { get; set; }
+        public SearchResultsModel SearchResults { get; set; }
+
+    }
+}
